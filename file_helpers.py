93d3b081728716c2dc4fe76884c15b054c119f51
Tini Tran <tinit@stud.ntnu.no>
2025-02-27 13:09:03 +0100

    Changed the save-file function...

--- file_helpers.py
+++ file_helpers.py
@@ -7,10 +7,10 @@ def allowed_file(filename, allowed_extensions):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in allowed_extensions
 
 
-def save_file(file, upload_folder):
+def save_file(filename, upload_folder):
     """Save the uploaded file to the specified folder."""
-    file_path = os.path.join(upload_folder, file.filename)
-    file.save(file_path)
+    file_path = os.path.join(upload_folder, filename)
+    #file.save(file_path)
     return file_path
 
 # def download_file(url, upload_folder):

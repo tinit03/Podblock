6909d57e70e3564454e5ed80c5198c0493fab70d
vildemv <vildemv@ntnu.no>
2025-03-17 09:41:37 +0100

    Did some changes to cache management added functionality for saving and retrieving urls and content

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from flask import Flask
 from flask_cors import CORS
 from config import Config
-from routes import audio_bp
-from cache_helpers import setup_cache
+from router import audio_bp
+from helpers.cache_helpers import setup_cache
+from helpers import url_helpers
 import redis
 
 app = Flask(__name__)
 app.config.from_object(Config)
 app.register_blueprint(audio_bp)
-redis_client = redis.Redis(host='localhost', port=6379, db=0)
-setup_cache(app)
+redis_client = redis.Redis(host='127.0.0.1', port=6379, db=0)
+setup_cache(app, redis_client)
 CORS(app)
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
